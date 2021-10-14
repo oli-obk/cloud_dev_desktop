aa5e1c8441229b66c39fecebefbca4b07db112c5
Oli Scherer <github35764891676564198441@oli-obk.de>
2021-10-14 13:51:50 +0200

    Don't run the setup script in sudo, instead run sudo on individual commands

--- setup_login_scheme.sh
+++ setup_login_scheme.sh
@@ -11,19 +11,19 @@ cargo build --all
 cd ..
 
 # Set up the auto-user creation
-cp team_login/target/debug/cronjob /etc/cron.create_all_team_users
-cat crontab_append >> /etc/crontab
+sudo cp team_login/target/debug/cronjob /etc/cron.create_all_team_users
+sudo cat crontab_append >> /etc/crontab
 
 # Set up the auto-login via ssh
-cp team_login/target/debug/team_login /etc/ssh/team_login
-cat sshd_append >> /etc/sshd_config
+sudo cp team_login/target/debug/team_login /etc/ssh/team_login
+sudo cat sshd_append >> /etc/sshd_config
 
 # The files that initially appear in a user's home dir
-cp skel/* /etc/skel/
+sudo cp skel/* /etc/skel/
 
 # Disable existing message of the day
-chmod -x /etc/update-motd.d/*
+sudo chmod -x /etc/update-motd.d/*
 
 # Add our own message of the day
 
-cp welcome-rust-dev /etc/update-motd.d/01-welcome-rust-dev
+sudo cp welcome-rust-dev /etc/update-motd.d/01-welcome-rust-dev
